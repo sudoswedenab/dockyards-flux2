49508273dbeabc485c3265294ff57ed866e2a549
Alexander Backlund <alexander.backlund@sudosweden.com>
2025-01-21 14:09:15 +0100

    Update tooling to generate workload template manifests

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
 package main
 
 //go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen rbac:roleName=dockyards-flux2 webhook paths="./..."
+//go:generate go run bitbucket.org/sudosweden/dockyards-flux2/cmd/generate-workloadtemplate-manifests
