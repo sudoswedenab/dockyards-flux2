e901b64d0e131324a4c83f152c7c151673c0fdc8
Alexander Backlund <alexander.backlund@sudosweden.com>
2025-02-17 14:04:49 +0100

    Add test helper for mocked custom resource definitions
    Using the test environment helper from the controller runtime package
    requires access to custom resource definitions for all dependencies.
    There does not really exist a good solution for accessing the custom
    resource definitions of the dependencies of this project.
    
    This adds a package with mocked custom resource definitions for the
    dockyards and flux custom resources. This ensures that the custom
    resources exists in the test environment but will allow any fields to be
    set. This is not an optimal solution but good enough to start with.

---
+++ test/mockcrds/mockcrds.go
@@ -0,0 +1,76 @@
+package mockcrds
+
+import (
+	dockyardsv1 "bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha3"
+	helmv2 "github.com/fluxcd/helm-controller/api/v2"
+	kustomizev1 "github.com/fluxcd/kustomize-controller/api/v1"
+	sourcev1 "github.com/fluxcd/source-controller/api/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+)
+
+var (
+	DockyardsCluster          = mockCRD(dockyardsv1.ClusterKind, "clusters", dockyardsv1.GroupVersion.Group, dockyardsv1.GroupVersion.Version)
+	DockyardsWorkload         = mockCRD(dockyardsv1.WorkloadKind, "workloads", dockyardsv1.GroupVersion.Group, dockyardsv1.GroupVersion.Version)
+	DockyardsWorkloadTemplate = mockCRD(dockyardsv1.WorkloadTemplateKind, "workloadtemplates", dockyardsv1.GroupVersion.Group, dockyardsv1.GroupVersion.Version)
+	HelmHelmRelease           = mockCRD(helmv2.HelmReleaseKind, "helmreleases", helmv2.GroupVersion.Group, helmv2.GroupVersion.Version)
+	KustomizeKustomization    = mockCRD(kustomizev1.KustomizationKind, "kustomizations", kustomizev1.GroupVersion.Group, kustomizev1.GroupVersion.Version)
+	SourceGitRepository       = mockCRD(sourcev1.GitRepositoryKind, "gitrepositories", sourcev1.GroupVersion.Group, sourcev1.GroupVersion.Version)
+	DockyardsWorktree         = mockCRD(dockyardsv1.WorktreeKind, "worktrees", dockyardsv1.GroupVersion.Group, dockyardsv1.GroupVersion.Version)
+
+	CRDs = []*apiextensionsv1.CustomResourceDefinition{
+		DockyardsCluster,
+		DockyardsWorkload,
+		DockyardsWorkloadTemplate,
+		DockyardsWorktree,
+		HelmHelmRelease,
+		KustomizeKustomization,
+		SourceGitRepository,
+	}
+)
+
+func mockCRD(kind, plural, group, version string) *apiextensionsv1.CustomResourceDefinition {
+	return &apiextensionsv1.CustomResourceDefinition{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: apiextensionsv1.SchemeGroupVersion.String(),
+			Kind:       "CustomResourceDefinition",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: plural + "." + group,
+		},
+		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+			Group: group,
+			Scope: apiextensionsv1.NamespaceScoped,
+			Names: apiextensionsv1.CustomResourceDefinitionNames{
+				Plural: plural,
+				Kind:   kind,
+			},
+			Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
+				{
+					Name:    version,
+					Served:  true,
+					Storage: true,
+					Subresources: &apiextensionsv1.CustomResourceSubresources{
+						Status: &apiextensionsv1.CustomResourceSubresourceStatus{},
+					},
+					Schema: &apiextensionsv1.CustomResourceValidation{
+						OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
+							Type: "object",
+							Properties: map[string]apiextensionsv1.JSONSchemaProps{
+								"spec": {
+									Type:                   "object",
+									XPreserveUnknownFields: ptr.To(true),
+								},
+								"status": {
+									Type:                   "object",
+									XPreserveUnknownFields: ptr.To(true),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
