f0bebdc59a956a0faf7a52a1a359d7e84c4335a1
Maxim Kravchenko <maxim.kravchenko@sudosweden.com>
2025-04-11 11:13:01 +0200

    Add prometheus operator types to cue module

--- tools.go
+++ tools.go
@@ -8,4 +8,5 @@ import (
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/api/types"
 	_ "github.com/RedHatInsights/strimzi-client-go/apis/kafka.strimzi.io/v1beta2"
+	_ "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 )
