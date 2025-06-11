34089b9a5ac24dd8dcdf29c825da2c5f96a2ba1a
Alexander Backlund <alexander.backlund@sudosweden.com>
2025-06-11 12:43:03 +0200

    Update controller-gen to be managed as tool dependency

--- go.mod
+++ go.mod
@@ -96,3 +96,5 @@ require (
 	sigs.k8s.io/structured-merge-diff/v4 v4.5.0 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
+
+tool sigs.k8s.io/controller-tools/cmd/controller-gen
--- tools.go
+++ tools.go
@@ -7,6 +7,5 @@ package main
 import (
 	_ "github.com/RedHatInsights/strimzi-client-go/apis/kafka.strimzi.io/v1beta2"
 	_ "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/api/types"
 )
